90f1e44a9ece3fd0a4d1be05f996001bb0462873
mancow <mancow@macfilez.net>
2018-11-24 09:22:52 -0600

    Update post-install.sh
    Ordering and commenting.

--- post-install.sh
+++ post-install.sh
@@ -1,14 +1,14 @@
-# enable sabnzbd at boot
-sysrc sabnzbd_enable=YES;
-
-# bounce sabnzbd to bootstrap /usr/local/sabnzbd
+# bounce sabnzbd once to bootstrap /usr/local/sabnzbd
 service sabnzbd onestart;
 sleep 5;
 service sabnzbd onestop;
 sleep 5;
 
-# listen on all interfaces, not just localhost
+# update configuration to listen on all interfaces
 sed -i '' "s/^host = 127.0.0.1/host = 0.0.0.0/g" /usr/local/sabnzbd/sabnzbd.ini;
+
+# really enable sabnzbd
+sysrc sabnzbd_enable=YES;
 
 # really start sabnzbd
 service sabnzbd start;
